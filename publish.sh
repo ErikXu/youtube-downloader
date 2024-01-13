3ba313c19c4567b64407db8544ade9536eed2e52
Erik Xu <81636759@qq.com>
2024-01-14 07:27:00 +0800

    Modify publish.sh.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm -rf ./publish_cli
+mkdir -p ./publish_cli
+if ! test -f ./publish_cli/yt-dlp_linux; then
+  wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -O ./publish_cli/yt-dlp_linux
+fi
+
+rm -f ./publish_cli/ytdl
 
 echo "Publish cli..."
 cd ./src/Cli
